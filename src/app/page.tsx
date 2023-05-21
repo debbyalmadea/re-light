55f1b90fcd9ae7a7feca48f46d1ac7ddffc8fc18
debbyalmadea <almadeaputri@gmail.com>
2023-05-21 22:55:51 +0700

    chg: description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,19 @@ import { faCode, faMinus } from "@fortawesome/free-solid-svg-icons";
 config.autoAddCss = false;
 
 const defaultCode = `
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
   title: "Syntax Highlighting - Python",
-  description:
-    "Python syntax highlighting using string matching algorithm and regular expression",
+  description: '
+    A Next.js-based syntax highlighter website with support for Python, Java, 
+    and JSX/TSX languages. Built using regular expressions, it automatically detects the 
+    language and highlights keywords, strings, comments, and more. 
+    Easily visualize and share code snippets with beautifully highlighted syntax.
+  '
 };
 
 export default function RootLayout({
