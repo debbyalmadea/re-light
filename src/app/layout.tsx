6a653e219630f2a484381ab47723b22750dad334
debbyalmadea <almadeaputri@gmail.com>
2023-05-22 16:18:50 +0700

    chg: web title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Syntax Highlighting - Python",
+  title: "Syntax Highlighter",
   description: `A Next.js-based syntax highlighter website with support for Python, Java, 
   and JSX/TSX languages. Built using regular expressions, it automatically detects the 
   language and highlights keywords, strings, comments, and more. 
